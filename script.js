351c6adf39b85162cc1c9ca080cdb9ab83806d74
Elena Cristescu <elenacristescu11@gmail.com>
2023-09-04 16:57:05 +0300

    example of scoping in practice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function calcAge (birthYear){
+    const age = 2023 - birthYear;
+   
+    function printAge(){
+        const output = `You are ${age}, born in ${birthYear}`
+        console.log(output);
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Ellen'
+calcAge(1991);

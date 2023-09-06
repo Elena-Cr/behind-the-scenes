de6d65bfd05928ffbb1ca2ae441fb9030a802e13
Elena Cristescu <elenacristescu11@gmail.com>
2023-09-06 16:08:42 +0300

    example of TMZ and Hoisting as well as what issues it can produce

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 'use strict';
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
-function calcAge (birthYear){
-    const age = 2023 - birthYear;
-   
-    function printAge(){
-        const output = `You are ${age}, born in ${birthYear}`
-        console.log(output);
-    }
-    printAge();
-    return age;
-}
 
-const firstName = 'Ellen'
-calcAge(1991);
+// var me = 'Ellen';
+// let job = 'teacher';
+// const year = 1991;
+
+
+//console.log(addArrow(2,3));
+console.log(addDecl(2,3));
+//console.log(addExpr(2,3));
+
+
+function addDecl(a,b){
+    return a + b;
+};
+const addExpr = function(a,b){
+    return a+b;
+};
+var addArrow = (a,b)=>a+b;
+
+
+if(!numProducts)deleteShopingCart();
+var numProducts = 10;
+
+function deleteShopingCart(){
+    console.log('All products deleted ');
+};
